4868f595449615ed7b704424a1ee7c92adb76996
Merek Orlowski <merekorlowski@gmail.com>
2018-02-09 15:54:24 -0500

    Initial commit

---
+++ src/client/src/components/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Engineering Outreach</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
