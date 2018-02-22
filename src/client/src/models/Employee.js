f89a8b0ec96f02a342c111af209d802965778751
mmayen <mewan074@uottawa.ca>
2018-02-22 17:50:33 -0500

    made changes
    I made the changes Merek...happy?

--- src/client/src/models/Employee.js
+++ src/client/src/models/Employee.js
@@ -1,11 +1,12 @@
 export default class Employee {
-    constructor(id, name) {
-    this.id=id;
-    this.name=name;
-    }
+    
+        constructor(id, name) {
+            this.id=id;
+            this.name=name;
+        }
 
-    Edit(id, name) {
-        this.id = id;
-        this.name = name;
-    }
+        edit(id, name) {
+            this.id = id;
+            this.name = name;
+        }
 }
