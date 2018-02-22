e7337bba715a53df8bda71c4977ee06ab27624a0
mmayen <mewan074@uottawa.ca>
2018-02-22 17:39:18 -0500

    employee
    created employee object

---
+++ src/client/src/models/Employee.js
@@ -0,0 +1,11 @@
+export default class Employee {
+    constructor(id, name) {
+    this.id=id;
+    this.name=name;
+    }
+
+    Edit(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+}
