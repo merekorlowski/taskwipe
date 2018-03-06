cb03d3acbd41f4635f9c20c2566a0ebf346b852b
mmayen <mewan074@uottawa.ca>
2018-03-06 12:24:14 -0500

    changes to employee branch
    check the changes please

--- src/client/src/models/Employee.js
+++ src/client/src/models/Employee.js
@@ -1,16 +1,17 @@
 export default class Employee {
-    
-    constructor(id, first_name, last_name, email) {
-        this.id=id;
-        this.firstName=first_name;
-        this.lastName=last_name;
-        this.email=email;
-    }
 
-    edit(id, first_name, last_name, email) {
-        this.id=id;
-        this.firstName=first_name;
-        this.lastName=last_name;
-        this.email=email;
-    }
+        constructor(id, first_name, last_name, email) {
+            this.id=id;
+            this.name=name;
+            this.firstName=first_name;
+            this.lastName=last_name;
+            this.email=email;
+        }
+
+        edit(id, first_name, last_name, email) {
+            this.id=id;
+            this.firstName=first_name;
+            this.lastName=last_name;
+            this.email=email;
+        }
 }
