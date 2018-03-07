d24a10ac03302db703114f6a9c08237e71bc4953
mmayen <mewan074@uottawa.ca>
2018-03-07 15:39:09 -0500

    indentation fxed

--- src/client/src/models/Employee.js
+++ src/client/src/models/Employee.js
@@ -1,15 +1,15 @@
 export default class Employee {
+    
+    constructor(id, first_name, last_name, email) {
+        this.id=id;
+        this.firstName=first_name;
+        this.lastName=last_name;
+        this.email=email;
+    }
 
-        constructor(id, first_name, last_name, email) {
-            this.id=id;
-            this.firstName=first_name;
-            this.lastName=last_name;
-            this.email=email;
-        }
-
-        modifyEmployee(first_name, last_name, email) {
-            this.firstName=first_name;
-            this.lastName=last_name;
-            this.email=email;
-        }
+    modifyEmployee(first_name, last_name, email) {
+        this.firstName=first_name;
+        this.lastName=last_name;
+        this.email=email;
+    }
 }
