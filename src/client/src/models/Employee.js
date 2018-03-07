c4a1ac52509a49adc1021d794ec90c080c04f286
mmayen <mewan074@uottawa.ca>
2018-03-07 15:27:15 -0500

    edit changed to modifyEmployee

--- src/client/src/models/Employee.js
+++ src/client/src/models/Employee.js
@@ -2,13 +2,12 @@ export default class Employee {
 
         constructor(id, first_name, last_name, email) {
             this.id=id;
-            this.name=name;
             this.firstName=first_name;
             this.lastName=last_name;
             this.email=email;
         }
 
-        edit(first_name, last_name, email) {
+        modifyEmployee(first_name, last_name, email) {
             this.firstName=first_name;
             this.lastName=last_name;
             this.email=email;
