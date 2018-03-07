e7ebff4c0ba4dd117f44a993b58939ad3d2d6b66
mmayen <mewan074@uottawa.ca>
2018-03-07 12:55:55 -0500

    updated employee model

--- src/client/src/models/Employee.js
+++ src/client/src/models/Employee.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-export default class Employee{
-=======
 export default class Employee {
->>>>>>> 2387db499b4552923ba251a81f4e28d0009c472a
 
         constructor(id, first_name, last_name, email) {
             this.id=id;
@@ -11,13 +7,8 @@ export default class Employee {
             this.lastName=last_name;
             this.email=email;
         }
-<<<<<<< HEAD
-        edit(first_name, last_name, email) {
-=======
 
-        edit(id, first_name, last_name, email) {
-            this.id=id;
->>>>>>> 2387db499b4552923ba251a81f4e28d0009c472a
+        edit(first_name, last_name, email) {
             this.firstName=first_name;
             this.lastName=last_name;
             this.email=email;
