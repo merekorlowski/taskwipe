7b619ccbe5084173b2e892291a9decc3a638a6bd
mmayen <mewan074@uottawa.ca>
2018-03-09 15:16:32 -0500

    indentation fixed
    i think i fixed it, please feedback

--- src/client/src/models/Employee.js
+++ src/client/src/models/Employee.js
@@ -1,25 +1,26 @@
-export default class Employee {
-    constructor(id, firstName, lastName, email) {
-        this.id=id;
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.email=email;
-        this.roles=[];
-    }
-
-    modify(firstName, lastName, email) {
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.email=email;
-    }
-    
-    assignRoles(role){
-        this.roles.push(role);
-    }
-    removeRoles(role){
-        var index =this.roles.indexOf(role);
-        if(index > -1){
-        this.roles.splice(index,1);
+   export default class Employee {
+        constructor(id, firstName, lastName, email) {
+            this.id=id;
+            this.firstName=firstName;
+            this.lastName=lastName;
+            this.email=email;
+            this.roles=[];
         }
-    }
-}
+
+        modify(firstName, lastName, email) {
+            this.firstName=firstName;
+            this.lastName=lastName;
+            this.email=email;
+        }
+
+        assignRoles(role){
+            this.roles.push(role);
+        }
+        
+        removeRoles(role){
+            var index =this.roles.indexOf(role);
+            if(index > -1){
+            this.roles.splice(index,1);
+            }
+        }
+   }
