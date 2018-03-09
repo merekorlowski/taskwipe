9ef488207e4cef5964bfb52ef2b7f21c6ec14565
mmayen <mewan074@uottawa.ca>
2018-03-09 13:42:29 -0500

    Update Employee.js

--- src/client/src/models/Employee.js
+++ src/client/src/models/Employee.js
@@ -1,14 +1,16 @@
 export default class Employee {
-    constructor(id, firstName, lastName, email) {
+    constructor(id, firstName, lastName, email, roles) {
         this.id=id;
         this.firstName=firstName;
         this.lastName=lastName;
         this.email=email;
+        this.roles=roles;
     }
 
-    modify(firstName, lastName, email) {
+    modify(firstName, lastName, email, roles) {
         this.firstName=firstName;
         this.lastName=lastName;
         this.email=email;
+        this.roles=roles;
     }
 }
