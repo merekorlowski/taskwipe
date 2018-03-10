921fabac63f0fb56fa14b00e8585e17f543bcfd7
Merek Orlowski <merekorlowski@gmail.com>
2018-03-09 22:27:31 -0500

    Added JSDoc comments

--- src/client/src/models/Task.js
+++ src/client/src/models/Task.js
@@ -1,5 +1,15 @@
 
 export default class Task {
+	/**
+	 * An Task
+	 * @constructor
+	 * @param {string} id - The id of this task
+	 * @param {string} title - The title of this task
+	 * @param {string} description - The description of this task
+	 * @param {string} priority - The priority of this task
+	 * @param {array} employees - The employees assigned to this task
+	 * @param {string} deadline - The deadline of this task
+	 */
 	constructor(id, title, description, priority, employees, deadline) {
 		this.id = id;
 		this.title = title;
@@ -15,11 +25,19 @@ export default class Task {
 		this.comments = [];
 	}
 
+	/**
+	 * Assign an employee to this task
+	 * @param {Employee} employee - The employee to assign to this task
+	 */
 	addEmployee(employee) {
 		this.employees.push(employee);
 		this.assigned = true;
 	}
 
+	/**
+	 * Unassign an employee from this task
+	 * @param {number} index - The index of the employee to unassing from this task
+	 */
 	removeEmployee(index) {
 		this.employees.splice(index, 1);
         
@@ -28,22 +46,45 @@ export default class Task {
 		}
 	}
 
+	/**
+	 * Add a time log to this task
+	 * @param {TimeLog} timelog - The time log to add to this task
+	 */
 	addTimeLog(timeLog) {
 		this.timeLogs.push(timeLog);
 	}
 
+	/**
+	 * Remove a time log from this task
+	 * @param {number} index - The index of the time log to remove from this task
+	 */
 	removeTimelog(index) {
 		this.timeLogs.splice(index, 1);
 	}
 
+	/**
+	 * Add a comment to this task
+	 * @param {string} comment - The comment to add to this task
+	 */
 	addComment(comment) {
 		this.comments.push(comment);
 	}
 
+	/**
+	 * Remove a comment from this task
+	 * @param {number} index - The index of the comment to remove from this task
+	 */
 	removeComment(index) {
 		this.comments.splice(index, 1);
 	}
 
+	/**
+	 * Modify the attributes of this task
+	 * @param {number} title - The new title of this task
+	 * @param {number} description - The new description of this task
+	 * @param {number} priority - The new priority of this task
+	 * @param {number} deadline - The new deadline of this task
+	 */
 	modify(title, description, priority, deadline) {
 		this.title = title;
 		this.description = description;
