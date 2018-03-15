1ecc03a4120f637cbb699b1312a3afe28b7b630d
Opeoluwa  Temitayo Eweje <ewejeopeoluwa@gmail.com>
2018-03-15 17:15:56 -0400

    Changed taskService to projectService

--- src/client/src/services/projects/test.js
+++ src/client/src/services/projects/test.js
@@ -1,19 +1,19 @@
-import TaskService from './index';
+import ProjectService from './index';
 
 describe('Test for creating new projects ', () => {
   it('Creates new project after create button is clicked.', () => {
-    let taskService = new TaskService();
+    let projectService = new ProjectService();
 
     let projectNames = [];
-    projectNames.push(taskService.getProjectNames());
+    projectNames.push(projectService.getProjectNames());
 
     let teamMembers = [];
-    teamMembers.push(taskService.getTeamMembers());
+    teamMembers.push(projectService.getTeamMembers());
 
     //Get all projectlist data in JSON format
     let projectLists;
-    for (var i=0; i<taskService.getProjectNames().length; i++) {
-      for (var j=0; j<taskService.getTeamMembers().length; j++) {
+    for (var i=0; i<projectService.getProjectNames().length; i++) {
+      for (var j=0; j<projectService.getTeamMembers().length; j++) {
         projectLists = {
           projectNames[i] : {
             'teamMember(s)' : teamMembers[j];
@@ -23,12 +23,12 @@ describe('Test for creating new projects ', () => {
     }
 
     //Create button gets clicked here, takes in user's input of project name and team member(s)
-    taskService.createProject(projectNames,teamMembers);
+    projectService.createProject(projectNames,teamMembers);
 
     //Test all project names and team members assigned in each project
-    for (var k=0; k<taskService.getProjects().length; k++) {
-      expect(taskService.getProjects[k].projectNames == projectLists.projectNames[k]);
-      expect(taskService.getProjects[k].projectNames.teamMembers == projectLists.projectNames[k].teamMembers);
+    for (var k=0; k<projectService.getProjects().length; k++) {
+      expect(projectService.getProjects[k].projectNames == projectLists.projectNames[k]);
+      expect(projectService.getProjects[k].projectNames.teamMembers == projectLists.projectNames[k].teamMembers);
     }
   });
 });
