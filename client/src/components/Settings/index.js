2e87231ac7a2a6b72d9df138e96ef0715509f8de
karim2506 <kbenh016@uottawa.ca>
2018-04-03 18:54:56 -0400

    cleaned up the styling for settings and fixed the checbkbox value update

--- client/src/components/Settings/index.js
+++ client/src/components/Settings/index.js
@@ -10,7 +10,7 @@ class Settings extends Component {
     this.state = {
       employeeId: '',
       numOfWeeksAhead: '',
-      showDeadlineForTask: '',
+      showDeadlineForTask: false,
       teamColor: '',
       teamName:''
     };
@@ -21,39 +21,35 @@ class Settings extends Component {
 
 	render() {
 		return (
-			<div className="container main-section">
-				<h2>Settings</h2>
-				<p>
+			<div className="container">
+        <p>
 					<button className="bg-theme-btn right" onClick={this.logout}>Logout</button>
 				</p>
+				<h2>Settings</h2>
 				<div className="title-underline bg-theme"></div>
         <ul className="list">
 					<li>
-            <h3 className="settings-title">Display Settings</h3>
-            <div className="settingsItem">
-              <span>Number of Weeks to Show in Tasks Page:</span>
-              <span className="settingsValue">
-                <input type="number" name="numOfWeeksAhead" value={this.state.numOfWeeksAhead} onChange={this.handleChange.bind(this)}/>
+            <div className="container">
+              <h3>Display Settings</h3>
+              <span className="col-xs-5">Number of Weeks to Show in Tasks Page:</span>
+              <span className="col-xs-5">
+                <input type="number" className="form-elem" name="numOfWeeksAhead" value={this.state.numOfWeeksAhead} onChange={this.handleChange.bind(this)}/>
               </span>
-            </div>
-            <div className="settingsItem">
-              <span>Show Deadlines in Tasks Page?</span>
-              <span className="settingsValue">
-                <input type="checkbox" checked={this.state.showDeadlineForTask} onChange={this.handleChange.bind(this)}/>
+              <span className="col-xs-5">Show Deadlines in Tasks Page?</span>
+              <span className="col-xs-5">
+                <input type="checkbox" className="form-elem" name="showDeadlineForTask" checked={this.state.showDeadlineForTask} onChange={this.handleChange.bind(this)}/>
               </span>
             </div>
           </li>
           <li>
-            <h3 className="settings-title">Team Settings</h3>
-            <div className="settingsItem">
-              <span>Team Name:</span>
-              <span className="settingsValue">
-                <input type="text" name="teamName" value={this.state.teamName} onChange={this.handleChange.bind(this)}/>
+            <div className="container">
+              <h3>Team Settings</h3>
+              <span className="col-xs-5">Team Name:</span>
+              <span className="col-xs-5">
+                <input type="text" className="form-elem" name="teamName" value={this.state.teamName} onChange={this.handleChange.bind(this)}/>
               </span>
-            </div>
-            <div className="settingsItem">
-              <span>Team Color:</span>
-              <span className="settingsValue">
+              <span className="col-xs-5">Team Color:</span>
+              <span className="col-xs-5">
                 <input type="color" name="teamColor" value={this.state.teamColor} onChange={this.handleChange.bind(this)}/>
               </span>
             </div>
@@ -68,10 +64,15 @@ class Settings extends Component {
 	 * @param {*} event
 	 */
 	handleChange(event) {
-    let settingsData = {...this.state.settingsData};
-    settingsData[event.target.name] = event.target.value;
-		// The change is stored in the change data structure
-    this.setState({settingsData: settingsData});
+    //this.setState({[event.target.name]: event.target.value});
+
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
 	}
   
   getUserSettings() {
