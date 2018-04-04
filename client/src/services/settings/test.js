5be4c1cb8dffbbcb7bb15ca434d5a28b85a9e220
karim2506 <kbenh016@uottawa.ca>
2018-04-03 21:35:36 -0400

    src/services/settings/test.js

---
+++ client/src/services/settings/test.js
@@ -0,0 +1,21 @@
+import SettingsService from './';
+
+describe('Settings API', () => {
+	let Service = new SettingsService();
+
+	it('tests "Number of weeks to be shown on Tasks Page" setting', (done) => {
+		done();
+	});
+
+	it('tests "Show deadlines in Tasks Page?" setting', (done) => {
+		done();
+	});
+
+	it('tests "Team name" setting', (done) => {
+		done();
+	});
+
+	it('tests "Team color" setting', (done) => {
+		done();
+	});
+});
