83917d254ee942ef950e3b3ed2de0c86297ac1a4
Merek Orlowski <merekorlowski@gmail.com>
2018-04-06 11:20:00 -0400

    Refactored code, separated client and webservice

---
+++ src/views/Tasks/DailyTasks.js
@@ -0,0 +1,226 @@
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import TaskService from '../../services/tasks';
+import ProjectService from '../../services/projects';
+import TaskListItem from './TaskListItem';
+import moment from 'moment';
+
+const DEFAULT_TYPE = 'Priority';
+
+class DailyTasks extends Component {
+	constructor(props) {
+		super(props);
+		this.newTask = {
+			title: '',
+			type: DEFAULT_TYPE,
+			employeeId: localStorage.getItem('employeeId'),
+			projectId: '',
+			comments: '',
+			date: this.props.date
+		};
+		this.state = {
+			displayDate: moment(this.props.date, 'YYYY-MM-DD').format('ddd, MMM DD'),
+			newTask: this.newTask,
+			tasks: this.props.tasks,
+			onGoingId: null,
+			projects: []
+		};
+		this.taskService = new TaskService();
+		this.projectService = new ProjectService();
+	}
+
+	componentDidMount() {
+		this.getProjects();
+	}
+
+	render() {
+		return (
+			<div>
+				<h5 className="display-date">{this.state.displayDate}</h5>
+				<ul className="list">
+					<li>
+						<form className="container" onSubmit={this.addTask.bind(this)}>
+							<span className="col-sm-11 col-md-5 col-lg-5">
+								<input name="title" autoComplete="off" type="text" autoFocus={this.isToday ? 'on' : ''} className="form-elem"
+									placeholder="Enter new task" required="true" value={this.state.newTask.title}
+									onChange={this.handleNewTaskChange.bind(this)}/>
+							</span>
+							<span className="col-sm-5 col-md-3 col-md-lg">
+								<select name="projectId" className="form-elem" value={this.state.newTask.projectId}
+									onChange={this.handleNewTaskChange.bind(this)}>
+									{this.state.projects.map((project, index) => (
+										<option key={index} value={project.projectId}>{project.title}</option>
+									))}
+								</select>
+							</span>
+							<span className="col-sm-3 col-md-2 col-lg-2">
+								<input name="deadline" type="date" className="form-elem" value={this.state.newTask.deadline}
+									onChange={this.handleNewTaskChange.bind(this)} />
+							</span>
+							<span className="col-sm-2 col-md-1 col-lg-1">
+								<button className="bg-theme-btn">
+									Create
+								</button>
+							</span>
+						</form>
+					</li>
+					{this.state.tasks.length > 0
+						? this.state.tasks.map((task, index) => (
+							<li id={task.taskId} key={task.taskId}>
+								<TaskListItem data={task}
+									handleDelete={this.deleteTask.bind(this, task.taskId, index)}
+									handleArchive={this.archiveTask.bind(this, task.taskId, index)}
+									handlePush={this.pushTask.bind(this, task.taskId, index)}
+									checkIfCanStart={this.canStartTask.bind(this, task.taskId)}
+									setOnGoingTask={this.setOnGoingTask.bind(this, task.taskId)}
+									removeOnGoingTask={this.removeOnGoingTask.bind(this)} />
+							</li>
+						))
+						: (
+							<li className="empty-list">
+								There are no tasks specified for this day.
+							</li>
+						)
+					}
+				</ul>
+			</div>
+		);
+	}
+
+	static get propTypes() {
+		return {
+			date: PropTypes.string.isRequired,
+			tasks: PropTypes.array.isRequired
+		};
+	};
+
+	/**
+	 * Updates the state when a value is changed
+	 * @param {*} event
+	 */
+	handleNewTaskChange(event) {
+		let newTask = {...this.state.newTask};
+		newTask[event.target.name] = event.target.value;
+		// The change is stored in the change data structure
+		this.setState({newTask: newTask});
+	}
+
+	get isToday() {
+		let today = moment().format('ddd, MMM DD');
+		return this.state.displayDate === today;
+	}
+
+	getProjects() {
+		this.projectService.getProjects(localStorage.getItem('employeeId')).then(res => {
+			let newTask = {...this.state.newTask};
+			newTask.projectId = res.data[0].projectId;
+			this.setState({
+				projects: res.data,
+				newTask: newTask
+			});
+		}).catch(err => {
+			console.error(err.message);
+		});
+	}
+
+	/**
+	 * Adds a task to the user's list of tasks
+	 * @param {*} task
+	 */
+	addTask(event) {
+		event.preventDefault();
+		this.taskService.addTask(this.state.newTask).then(res => {
+			// if ther response is ready, update the task in the list
+			let tasks = this.state.tasks;
+			tasks.push(res.data);
+			this.setState({
+				tasks: tasks,
+				newTask: {...this.newTask}
+			});
+		}).catch(err => {
+			console.error(err.message);
+		});
+	}
+
+	/**
+	 * Updates a task from the user's list of tasks
+	 * @param {*} task
+	 * @param {number} index
+	 */
+	updateTask(task, index) {
+		this.taskService.updateTask(task).then(res => {
+			// if ther response is ready, update the task in the list
+			let tasks = this.state.tasks;
+			tasks[index] = res.data;
+			this.setState({tasks: tasks});
+		}).catch(err => {
+			console.error(err.message);
+		});
+	}
+
+	/**
+	 * Deletes a task from the user's list of tasks
+	 * @param {string} taskId
+	 * @param {number} index
+	 */
+	deleteTask(taskId, index) {
+		this.taskService.deleteTask(taskId).then(res => {
+			// if ther response is ready, update the task in the list
+			let tasks = this.state.tasks;
+			tasks.splice(index, 1);
+			this.setState({tasks: tasks});
+		}).catch(err => {
+			console.error(err.message);
+		});
+	}
+
+	/**
+	 * Archives a task from the user's list of tasks
+	 * @param {string} taskId
+	 * @param {number} index
+	 */
+	archiveTask(taskId, index) {
+		this.taskService.archiveTask(taskId).then(res => {
+			let tasks = this.state.tasks;
+			tasks.splice(index, 1);
+			this.setState({tasks: tasks});
+		}).catch(err => {
+			console.error(err.message);
+		});
+	}
+
+	/**
+	 * Pushes a task to the next day
+	 * @param {string} taskId
+	 * @param {number} index
+	 */
+	pushTask(taskId, index) {
+		this.taskService.pushTask(taskId).then(res => {
+			let tasks = this.state.tasks;
+			tasks.splice(index, 1);
+			this.setState({tasks: tasks});
+		}).catch(err => {
+			console.error(err.message);
+		});
+	}
+
+	canStartTask() {
+		if (!this.state.onGoingId && this.isToday) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	setOnGoingTask(taskId) {
+		if (this.canStartTask()) {
+			this.setState({onGoingId: taskId});
+		}
+	}
+
+	removeOnGoingTask() {
+		this.setState({onGoingId: null});
+	}
+}
+
+export default DailyTasks;
---
+++ src/views/Tasks/TaskListItem.js
@@ -0,0 +1,258 @@
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import TaskService from '../../services/tasks';
+import ProjectService from '../../services/projects';
+import moment from 'moment';
+import './styles.css'
+
+class TaskListItem extends Component {
+	/**
+	 * A Task
+	 * @constructor
+	 * @param {JSON} props
+	 */
+	constructor(props) {
+		super(props);
+		let task = this.props.data;
+		this.state = {
+			taskId: task.taskId,
+			title: task.title,
+			type: task.type,
+			projectId: task.projectId,
+			comments: task.comments,
+			projects: [],
+			onGoingTime: null,
+			isExpanded: false,
+			mouseover: false
+		};
+		this.taskService = new TaskService();
+		this.projectService = new ProjectService();
+	}
+
+	/**
+	 * Static getter for validating the properties passed into this component
+	 */
+	static get propTypes() {
+		return {
+			data: PropTypes.any.isRequired,
+			handleDelete: PropTypes.func.isRequired,
+			handleArchive: PropTypes.func.isRequired,
+			handlePush: PropTypes.func.isRequired,
+			checkIfCanStart: PropTypes.func.isRequired,
+			setOnGoingTask: PropTypes.func.isRequired,
+			removeOnGoingTask: PropTypes.func.isRequired
+		};
+	};
+
+	componentDidMount() {
+		this.getOnGoingTimeLog();
+		this.getProjects();
+	}
+
+	/**
+	 * Renders the task list item
+	 */
+	render() {
+		return (
+			<div className={`container task-list-item ${this.state.isExpanded ? 'expanded': ''}`}>
+				<span className="col-md-1 col-lg-1 col-sm-1" onClick={this.toggleExpand.bind(this)}>
+					<i className={`fa ${this.state.isExpanded ? 'fa-angle-up' : 'fa-angle-down'} expand-icon`}></i>
+				</span>
+				<span className="col-md-4 col-lg-4 col-sm-11">
+					<span className="task-title">{this.state.title}</span>
+				</span>
+				<span className="col-md-3 col-lg-3 col-sm-5">
+					<select name="projectId" className="form-elem" value={this.state.projectId} onChange={this.handleChange.bind(this)}>
+						{this.state.projects.map((project, index) => (
+							<option key={index} value={project.projectId}>{project.title}</option>
+						))}
+					</select>
+				</span>
+				<span className="col-md-2 col-lg-2 col-sm-3">
+					<select name="type" className="form-elem" value={this.state.type} onChange={this.handleChange.bind(this)}>
+						<option value="Priority">Priority</option>
+						<option value="Push">Push</option>
+						<option value="Archive">Archive</option>
+						<option value="Optional">Optional</option>
+						<option value="Delete">Delete</option>
+					</select>
+				</span>
+				<span className="col-md-1 col-lg-1 col-sm-2">
+					{this.state.onGoingTime === null
+						? (<button className={`bg-theme-btn ${!this.checkIfCanStart ? 'disabled-btn' : ''}`}
+							onClick={this.startTimer.bind(this)} disabled={!this.checkIfCanStart}>Start</button>)
+						: (<button className="bg-theme-btn on-going-time"
+							onClick={this.stopTimer.bind(this)}
+							onMouseEnter={this.onMouseEnter.bind(this)}
+							onMouseLeave={this.onMouseLeave.bind(this)}>
+							{this.state.mouseover ? 'Stop' : this.state.onGoingTime}
+						</button>)}
+				</span>
+				<div className="col-xs-11 list-elem-details">
+					<textarea name="comments"  autoFocus="on" placeholder="Notes"
+						value={this.state.comments} onChange={this.handleChange.bind(this)}></textarea>
+				</div>
+			</div>
+		);
+	}
+
+	/**
+	 * Updates the state when a value is changed
+	 * @param {*} event
+	 */
+	handleChange(event) {
+		if (event.target.name === 'type' && event.target.value === 'Delete') {
+			this.handleDelete();
+		} else if (event.target.name === 'type' && event.target.value === 'Archive') {
+			this.handleArchive();
+		} else {
+			this.taskService.updateTask(this.state.taskId, event.target.name, event.target.value).then(res => {
+				this.setState({
+					[res.data.attribute]: res.data.value
+				});
+			}).catch(err => {
+				console.error(err.message);
+			});
+		}
+	}
+
+	/**
+	 * Calls the parent's function to handle deleting this task
+	 */
+	handleDelete() {
+		this.props.handleDelete();
+	}
+
+	handleArchive() {
+		this.props.handleArchive();
+	}
+
+	handlePush() {
+		this.props.handlePush();
+	}
+
+	/**
+	 * Getter that calls the parent's function to verify if the user can start this task
+	 */
+	get checkIfCanStart() {
+		return this.props.checkIfCanStart();
+	}
+	
+	/**
+	 * Toggles the expand feature of the task
+	 */
+	toggleExpand() {
+		let isExpanded = this.state.isExpanded;
+		this.setState({isExpanded: !isExpanded});
+	}
+
+	/**
+	 * Update the mouseover flag when the user moves the mouse over of the timer button
+	 */
+	onMouseEnter() {
+		this.setState({mouseover: true});
+	}
+
+	/**
+	 * Update the mouseover flag when the user moves the mouse off of the timer button
+	 */
+	onMouseLeave() {
+		this.setState({mouseover: false});
+	}
+
+	/**
+	 * Retrieves the last started time and starts an interval to display current time in minutes
+	 */
+	getOnGoingTimeLog() {
+		this.taskService.getOnGoingTimeLog(this.state.taskId).then(res => {
+			if (res.data.startTime) {
+				this.props.setOnGoingTask(this.state.taskId);
+				this.setDuration(res.data.startTime);
+				this.onGoingInterval = setInterval(() => {
+					this.setDuration(res.data.startTime);
+				}, 1000);
+			}
+		}).catch(err => {
+			console.error(err.message);
+		});
+	}
+
+	getProjects() {
+		this.projectService.getProjects(localStorage.getItem('employeeId')).then(res => {
+			this.setState({projects: res.data});
+		}).catch(err => {
+			console.error(err.message);
+		});
+	}
+
+	/**
+	 * Retrieves all of the time logs for this task
+	 */
+	getTimeLogs() {
+		this.taskService.getTimeLogs(this.state.taskId).then(res => {
+			this.setState({timeLogs: res.data});
+		}).catch(err => {
+			console.error(err.message);
+		});
+	}
+
+	/**
+	 * Starts a new timer
+	 */
+	startTimer() {
+		this.taskService.startTimer(this.state.taskId).then(res => {
+			this.props.setOnGoingTask(this.state.taskId);
+			this.setDuration(res.data.startTime);
+			this.onGoingInterval = setInterval(() => {
+				this.setDuration(res.data.startTime);
+			}, 1000);
+			this.setState({mouseover: true});
+		}).catch(err => {
+			console.error(err.message);
+		});
+	}
+
+	/**
+	 * Stops the current running timer
+	 */
+	stopTimer() {
+		this.taskService.stopTimer(this.state.taskId).then(res => {
+			this.props.removeOnGoingTask();
+			this.setState({
+				onGoingTime: null
+			});
+			clearInterval(this.onGoingInterval);
+		}).catch(err => {
+			console.error(err.message);
+		});
+	}
+
+	/**
+	 * Helper method for setting the duration of the current time since the given start time
+	 * @param {string} startTime
+	 */
+	setDuration(startTime) {
+		let ms = moment().diff(moment(startTime, 'YYYY-MM-DD HH:mm:ss'));
+		let d = moment.duration(ms);
+		let s = Math.floor(d.asHours()) + moment.utc(ms).format(':mm:ss');
+
+		this.setState({
+			onGoingTime: s
+		});
+	}
+
+	/**
+	 * Used for sending the JSON of this task to the server
+	 */
+	toJSON() {
+		return {
+			taskId: this.state.taskId,
+			title: this.state.title,
+			type: this.state.type,
+			projectId: this.state.projectId,
+			comments: this.state.comments
+		};
+	}
+}
+
+export default TaskListItem;
