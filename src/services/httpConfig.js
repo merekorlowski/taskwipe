74704fcda1d4191abc93e911d09403d6ba2a9259
Merek Orlowski <merekorlowski@gmail.com>
2018-04-08 15:26:30 -0400

    Added environment env in http config

--- src/services/httpConfig.js
+++ src/services/httpConfig.js
@@ -1,7 +1,7 @@
 
 export default {
 	// host: 'http://ec2-18-219-180-211.us-east-2.compute.amazonaws.com',
-	host: 'http://localhost',
+	host: env.host ? env.host : 'http://localhost',
 	basePath: 'api',
 	port: 3001
 };
