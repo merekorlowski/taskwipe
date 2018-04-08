2f4e48bcd1b116de363491a643ae6a510387e35f
Merek Orlowski <merekorlowski@gmail.com>
2018-04-08 15:28:51 -0400

    Fixed error

--- src/services/httpConfig.js
+++ src/services/httpConfig.js
@@ -1,7 +1,7 @@
 
 export default {
 	// host: 'http://ec2-18-219-180-211.us-east-2.compute.amazonaws.com',
-	host: env.host ? env.host : 'http://localhost',
+	host: process.env.host ? process.env.host : 'http://localhost',
 	basePath: 'api',
 	port: 3001
 };
